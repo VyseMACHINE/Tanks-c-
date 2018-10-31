a360750b00a296c5b56a5a208a516e1ba8ac1556
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2018-10-31 16:55:26 +0600

    Add files via upload

---
+++ dz csharp/dz3/Tank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3
+{
+    public class Tank
+    {
+        protected string name;
+        protected int boekomplekt;
+        protected int lvlBroni;
+        protected int lvlManevr;
+        public Tank(int boekomplekt, int lvlBroni, int lvlManevr, string name)
+        {
+
+            this.boekomplekt = boekomplekt;
+            this.lvlBroni = lvlBroni;
+            this.lvlManevr = lvlManevr;
+            this.name = name;
+        }
+        public Tank()
+        {
+            boekomplekt = 0;
+            lvlBroni = 0;
+            lvlManevr = 0;
+            name = "null";
+        }
+        public void Print()
+        {
+            Console.WriteLine("name - " + name);
+            Console.WriteLine("boekomplekt = " + boekomplekt + " lvlBroni = " + lvlBroni + " lvlManevr = " + lvlManevr);
+        }
+
+        public static Tank operator *(Tank T34, Tank pantera)
+        {
+            if (T34.boekomplekt > pantera.boekomplekt && T34.lvlBroni > pantera.lvlBroni)
+            {
+                return T34;
+            }
+            if (T34.boekomplekt > pantera.boekomplekt && T34.lvlManevr > pantera.lvlManevr)
+            {
+                return T34;
+            }
+            if (T34.lvlBroni > pantera.lvlBroni && T34.lvlManevr > pantera.lvlManevr)
+            {
+                return T34;
+            }
+            else
+            {
+                return pantera;
+            }
+        }
+    }
+}
